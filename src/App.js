55341110c890d95fbee3d7fbf1d6a30acf00c11c
hong <hong@aftership.net>
2016-08-30 17:14:31 +0800

    inital release

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import Countdown from 'react-count-down'
+
+class App extends Component {
+  render() {
+	  let OPTIONS = { endDate: '09/01/2016 10:00 AM'};
+    return (
+      <div className="App">
+			<div className="logo--small"></div>
+        <Countdown options={OPTIONS} />
+		  <p className="prefix">until new secure.aftership.com is launch.</p>
+      </div>
+    );
+  }
+}
+
+export default App;
